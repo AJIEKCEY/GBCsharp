474dff1f2b7ba01f5e70407cb3aa1d26dde36c6c
AJIEKCEY <sergheialexei@gmail.com>
2020-12-15 01:16:31 +0300

    Lesson 2

---
+++ lesson2/lesson2/ex2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер текущего месяца");
+            int monthNumber = Convert.ToInt32(Console.ReadLine());
+            string monthName;
+
+            switch (monthNumber)
+            {
+                case 1:
+                    monthName = "Январь";
+                    break;
+                case 2:
+                    monthName = "Февраль";
+                    break;
+                case 3:
+                    monthName = "Март";
+                    break;
+                case 4:
+                    monthName = "Апрель";
+                    break;
+                case 5:
+                    monthName = "Май";
+                    break;
+                case 6:
+                    monthName = "Июнь";
+                    break;
+                case 7:
+                    monthName = "Июль";
+                    break; 
+                case 8:
+                    monthName = "Август";
+                    break;   
+                case 9:
+                    monthName = "Сентябрь";
+                    break;                
+                case 10:
+                    monthName = "Октябрь";
+                    break;                
+                case 11:
+                    monthName = "Ноябрь";
+                    break;                
+                case 12:
+                    monthName = "Декабрь";
+                    break;
+                default:
+                    monthName = "Вести можно только числа от 1 до 12!";
+                    break;
+            }
+            Console.WriteLine($"{monthName}");
+        }
+    }
+}
---
+++ lesson2/lesson2/ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер месяца: ");
+            byte monthNumber = Convert.ToByte(Console.ReadLine());
+            
+            Console.Write("Введите температуру: ");
+            int temp = Convert.ToInt32(Console.ReadLine());
+
+            if ((monthNumber == 1 || monthNumber == 2 || monthNumber == 12) && temp > 0 )
+            {
+                Console.WriteLine("Дождливая зима");
+            }
+            else
+            {
+                Console.WriteLine("Спасибо, эти данные невероятно важны для нас!");
+            }
+        }
+    }
+}
